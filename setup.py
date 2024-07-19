3f4c7efbac5aae9f802a7389bd98c9ab924c1ed2
nathgrin <6354761+nathgrin@users.noreply.github.com>
2024-07-19 23:18:47 +0200

    make setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(name='FindLightApp',
+      version='1.0',
+      description='Find light app',
+      author='NJG',
+      author_email='',
+      url='https://github.com/nathgrin/FindLightApp',
+      packages=[''],
+     )
